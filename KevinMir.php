5ab11e5ff523037989c41c272cd1af4ae8c9211a
Kevin Mir <kevinmir6@gmail.com>
2025-03-27 14:42:04 -0300

    Aprendimos a utilizar variables

---
+++ KevinMir.php
@@ -0,0 +1,20 @@
+<?php echo "Hola mundo";
+// esto imprime "Hola mundo"
+# esto imprime "Hola mundo"
+/* esto imprime "Hola mundo" */
+$edad = 18;
+$nombre_del_cliente = "Felipe";
+$nombre_del_cliente = 10;
+
+$nombre = "Manolo";// Texto. Puede ser con comillas simples o dobles (String)
+$edad = 31; //Enteros (Integer)
+$altura = 1.72; //Decimales, usando el punto en lugar de la coma (Float)
+$mayorEdad = true; // Verdad o falso (Boolean)
+
+//$nombreCompleto = "Juan Pérez" // Falta colocar ";" al final.
+//$Edad = 25; // Está  correcto.
+//$estAtura = 1.75; // Está bien, solo cambiaría "$estAtura" por "$estatura" o "$ESTATURA".
+//$numero = "10"; //Especificaría de qué es el número. Ejemplo: "$numero_de_camiseta" y le quitaría las comillas al "10".
+//Apellido = ´Fernández´; // Cambiaría las (´´) por comillas simples ('') o dobles ("").
+//pais_de_origen= "Uruguay" // Falta colocar "$" para especificar que "pais_de_origen" es en realidad una variable, además de colocar ";" al final.
+//$salario = $2000; //No se puede colocar "$2000" como valor de la variable, solo colocar "2000".
